b742d4423e09a82352456d06bffb4f8177f97461
Augustolhama <gutoeicardo2009@gmail.com>
2023-04-27 20:50:13 -0300

    Está dando algum erro na linha 41. Augusto

---
+++ EP2-PreencheFrota.py
@@ -0,0 +1,35 @@
+def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
+    frota_anteriores=[]
+    #Função que define a posição dos navios
+    def define_posicoes(linha,coluna,orientacao,tamanho):
+        lista=[]
+        i=0
+
+        if tamanho==1:
+            return [[linha,coluna]]
+        
+        if orientacao == 'vertical':
+            while i<tamanho:
+                lista.append([linha+i,coluna])
+                
+                i=i+1
+            
+        
+        if orientacao == 'horizontal':
+            while i<tamanho:
+                lista.append([linha,coluna+i])
+                
+                i=i+1
+
+        return lista  
+
+    for i in frota.keys():
+        if nome_navio==i:
+            frota_anteriores=frota[i]
+    
+    if len(frota_anteriores)>0:
+        frota={nome_navio:[define_posicoes(linha,coluna,orientacao,tamanho),frota_anteriores]}
+    else:
+        frota={nome_navio:[define_posicoes(linha,coluna,orientacao,tamanho)]}
+    
+    return frota
