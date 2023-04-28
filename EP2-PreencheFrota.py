5d7e94febf303c384ad39e3551521995203f3e39
Augustolhama <gutoeicardo2009@gmail.com>
2023-04-27 21:45:30 -0300

    Tentativa 2/ Augusto Ferreira

--- EP2-PreencheFrota.py
+++ EP2-PreencheFrota.py
@@ -28,8 +28,8 @@ def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
             frota_anteriores=frota[i]
     
     if len(frota_anteriores)>0:
-        frota={nome_navio:[define_posicoes(linha,coluna,orientacao,tamanho),frota_anteriores]}
+        frota[nome_navio]=[frota_anteriores,define_posicoes(linha,coluna,orientacao,tamanho)]
     else:
-        frota={nome_navio:[define_posicoes(linha,coluna,orientacao,tamanho)]}
+        frota[nome_navio]=[define_posicoes(linha,coluna,orientacao,tamanho)]
     
     return frota
