6ef591e908365cafa2c0c155efe928dcec4d17d2
Augustolhama <gutoeicardo2009@gmail.com>
2023-05-02 12:03:16 -0300

    EP2-Preenche  Frota/ Augusto Ferreira

--- EP2-PreencheFrota.py
+++ EP2-PreencheFrota.py
@@ -1,36 +1,32 @@
+#Função que define a posição dos navios
+def define_posicoes(linha, coluna, orientacao, tamanho):
+  lista=[]
+  i=0
+
+  if tamanho==1:
+      return [[linha, coluna]]
+  
+  if orientacao == 'vertical':
+    while i < tamanho:
+        lista.append([linha+i, coluna])
+        i += 1
+      
+  
+  if orientacao == 'horizontal':
+    while i < tamanho:
+      lista.append([linha, coluna+i])
+      i += 1
+
+  return lista 
+
 def preenche_frota(frota, nome_navio, linha, coluna, orientacao, tamanho):
-    frota_anteriores=[]
-    
-    #Função que define a posição dos navios
-    def define_posicoes(linha,coluna,orientacao,tamanho):
-        lista=[]
-        i=0
+  x=[]
+  x.append(define_posicoes(linha,coluna,orientacao,tamanho))
+  
+  if nome_navio not in frota:
+    frota[nome_navio]= x
+  else:
+    tudo=frota[nome_navio]+x
+    frota[nome_navio] = tudo
 
-        if tamanho==1:
-            return [[linha,coluna]]
-        
-        if orientacao == 'vertical':
-            while i<tamanho:
-                lista.append([linha+i,coluna])
-                
-                i=i+1
-            
-        
-        if orientacao == 'horizontal':
-            while i<tamanho:
-                lista.append([linha,coluna+i])
-                
-                i=i+1
-
-        return lista    
-
-    for i in frota.keys():
-        if nome_navio==i:
-            frota_anteriores=frota[i]
-    
-    if len(frota_anteriores)>0:
-        frota[nome_navio]=define_posicoes(linha,coluna,orientacao,tamanho),frota_anteriores
-    else:
-        frota[nome_navio]=define_posicoes(linha,coluna,orientacao,tamanho)
-    
-    return frota
+  return frota
